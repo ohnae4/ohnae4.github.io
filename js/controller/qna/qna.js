656a1913343fcca6a75b3a7825f931c7e8346322
kaisa <7083620@hanmail.net>
2018-07-15 14:14:48 +0900

    ..

--- js/controller/qna/qna.js
+++ js/controller/qna/qna.js
@@ -9,32 +9,25 @@
 		//rvQna
     	$scope.rvQna = {};
 		
-    	$scope.ROOM_QNA = {
+    	//QNA
+    	$scope.QNA = {
     		QNA_NUMBER : null,
 			MEMBER_NAME : '최재호',
 			MEMBER_PHONE : '01055555552',
 			MEMBER_EMAIL : '232@email.com',
 			PASSWORD : '111',
-			SUBJECT : 2,
-			CONTENT : '',
-			CREATE_DATE : ''
+			SUBJECT : '제목',
+			CONTENT : '내용'
 		};
     	
-    	$scope.$watch('ROOM_QNA.PERSONNEL_1', function(val){
-    		if(val){
-    			var int = parseInt(val) - parseInt($scope.rvQna.selectRoom.MIN_PERSONNEL);
-    			var price = parseInt($scope.rvQna.selectRoom['PRICE_'+ $scope.rvQna.selectDay.priceCode]);
-    			$scope.ROOM_QNA.PRICE = price + int;
-    		}
-        },true);
-    	
+    	//문의하기
     	$scope.layerQna = {
-    		active : false,
+    		active : true,
     		close : function(){
     			this.active = false;
     		},
     		submit : function(){
-				$http.jsonp(kaisaApi.setQna + $scope.jsonpParam($scope.ROOM_QNA)).success(function(data){
+				$http.jsonp(kaisaApi.setQna + $scope.jsonpParam($scope.QNA)).success(function(data){
 					if(data.success){
 						$scope.getQnaList();
 						$scope.layerQna.close();
@@ -71,24 +64,12 @@
 				return Math.ceil($scope.qnaList.length / this.pageSize);
 			}
     	};
-		//객실 예약정보
+		//문의리스트
     	$scope.qnaList = {};
     	$scope.getQnaList = function(){
     		$scope.loading.active = true;
-			$http.jsonp(kaisaApi.getQnaList + $scope.jsonpParam({ searchDateYear : $filter('date')($scope.rvQna.start.date,'yyyy') , searchDateMonth : $filter('date')($scope.rvQna.start.date,'MM') })).success(function(data){
+			$http.jsonp(kaisaApi.getQnaList + $scope.jsonpParam({  })).success(function(data){
 				$scope.qnaList = data.items;
-				$scope.rvQna.start.date = new Date(data.date);
-				$scope.rvQna.start.now = new Date(data.now);
-				$scope.rvQna.init();
-				$scope.rvQna.start.month = $filter('date')($scope.rvQna.start.date,'MM');
-				for(var i in $scope.qnaList){
-					$scope.qnaList[i].ROOM_STATUS_CODE_ORIGIN = $scope.qnaList[i].ROOM_STATUS_CODE;
-					for(var j in $scope.rvQna.start.day){
-						if($scope.qnaList[i].QNA_DATE.substring(8,10) == $scope.rvQna.start.day[j].date){
-							$scope.rvQna.start.day[j].qna.push($scope.qnaList[i]);
-						}
-					}
-				}
 				$scope.loading.active = false
 		    }).error(function(data){
 		    	$scope.alert.open({message : '객실 예약정보 조회 실패.'});
@@ -96,20 +77,7 @@
 		    });
     	};
     	$scope.getQnaList();
-    	$scope.updateQna = function(no,code){
-    		$http.jsonp(kaisaApi.updateQna + $scope.jsonpParam({ QNA_NUMBER : no , ROOM_STATUS_CODE : code })).success(function(data){
-    			if(data.success){
-    				$scope.alert.open({message : data.message});
-    				$scope.getQnaList();
-    			}else{
-    				$scope.alert.open({message : data.message});
-    			}
-				$scope.loading.active = false
-		    }).error(function(data){
-		    	console.log('update error');
-		    	$scope.loading.active = false;
-		    });
-    	};
+    	
     	$scope.deleteQna = { //<!-- <button type="button" class="normal" data-ng-click="deleteQna.click(i.QNA_NUMBER)">삭제</button> -->
     		no : null,
     		callback : function(){
