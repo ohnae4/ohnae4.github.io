90b90bbb94dd18f46ebd9bcdbcd9188de626a793
kaisa <ohnae4@gmail.com>
2022-07-09 08:40:50 +0900

    ..

--- js/controller/front/main.js
+++ js/controller/front/main.js
@@ -9,11 +9,11 @@
 		};
 		$scope.visualList = [
 			{url : '/img/visual/large/o1.png'},
-			{url : '/img/visual/large/o2.png'},
+			//{url : '/img/visual/large/o2.png'},
 			{url : '/img/visual/large/o3.png'},
 			{url : '/img/visual/large/o4.png'},
 			{url : '/img/visual/large/o5.png'},
-			{url : '/img/visual/large/o6.png'},
+			//{url : '/img/visual/large/o6.png'},
 			{url : '/img/visual/large/o7.png'}
 			// {url : '/img/visual/large/o8.png'}
 		];
